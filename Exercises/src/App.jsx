3da92999f993fe1216ce2ae80eab32f1beb71aa6
huenguyensw <hue.blue@gmail.com>
2023-03-05 15:50:12 +0100

    exercise1-02

--- Exercises/src/App.jsx
+++ Exercises/src/App.jsx
@@ -5,16 +5,27 @@ import Hero from './Components/Exercise02/Hero'
 import Portfolio from './Components/Exercise02/Portfolio'
 import About from './Components/Exercise02/About'
 import Contact from './Components/Exercise02/Contact'
+import Header from './Components/Exercise01/Header'
+import Sidebar from './Components/Exercise01/Sidebar'
+import Main from './Components/Exercise01/Main'
+import Footer from './Components/Exercise01/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Hero />
+      {/* Exercise 1 */}
+      <Header/>
+      <Sidebar/>
+      <Main/>
+      <Footer/>
+
+      {/* Exercise 2 */}
+      {/* <Hero />
       <Portfolio />
       <About />
-      <Contact />
+      <Contact /> */}
     </div>
   )
 }
