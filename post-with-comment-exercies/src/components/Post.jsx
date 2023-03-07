ec1b03d9f9666b7862cbe1a9c6c24c9e0a91aedb
huenguyensw <hue.blue@gmail.com>
2023-03-08 00:04:05 +0100

    commit

--- post-with-comment-exercies/src/components/Post.jsx
+++ post-with-comment-exercies/src/components/Post.jsx
@@ -2,21 +2,21 @@ import React, { useState } from 'react'
 import CommentSection from './CommentSection'
 import UserInfo from './UserInfo'
 
-const Post = (props) => {
+const Post = ({name,comment}) => {
     const [displayComment, setDisplayComment] = useState(true);
     const toggleState = () => {
         setDisplayComment(!displayComment)
     }
     return (
         <div className='post-area'>
-            <h1>{props.name.headline}</h1>
-            {props.name.date.toISOString().substring(0, 10)}
-            <p>{props.name.content}</p>
-            <UserInfo author={props.name.author} />
+            <h1>{name.headline}</h1>
+            {name.date.toISOString().substring(0, 10)}
+            <p>{name.content}</p>
+            <UserInfo author={name.author} />
             <br />
             <button onClick={toggleState}>Show/hide comments</button>
             <br />
-            {displayComment ? <CommentSection note={props.comment} /> : null}
+            {displayComment ? <CommentSection note={comment} /> : null}
 
         </div>
     )
